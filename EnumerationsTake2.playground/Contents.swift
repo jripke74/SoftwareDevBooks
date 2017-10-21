76ca1722d2459d875fe851cc2595b54f9767e7e8
Jeff Ripke <jripke74@gmail.com>
2017-10-21 07:37:58 -0700

    EnumeraionsTake2: add switch for TextAlignment

--- EnumerationsTake2.playground/Contents.swift
+++ EnumerationsTake2.playground/Contents.swift
@@ -2,5 +2,24 @@ enum TextAlignment {
     case left
     case right
     case center
-    case justified
+    case justify
+}
+
+var alignment = TextAlignment.justified
+alignment = .right
+
+//if alignment == .right {
+//    print("We should right-align the text.")
+//}
+
+// A switch statement would be better
+switch alignment {
+case .left:
+    print("left aligned")
+case .right:
+    print("right aligned")
+case .center:
+    print("center aligned")
+case .justify:
+    print("justified")
 }
