ab29a28a6a40f95f22b54d71759dc9fd58a59912
Jeff Ripke <jripke74@gmail.com>
2017-11-08 14:56:00 -0800

    MonsterTownII: added Struct Town and called in main

---
+++ MonsterTownII/MonsterTownII/Town.swift
@@ -0,0 +1,14 @@
+//
+//  Town.swift
+//  MonsterTownII
+//
+//  Created by Jeff Ripke on 11/8/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+struct Town {
+    var population = 5_422
+    var numberOfStoplights = 4
+}
---
+++ MonsterTownII/MonsterTownII/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTownII
+//
+//  Created by Jeff Ripke on 11/8/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+var myTown = Town()
+print("Population: \(myTown.population), number of stoplights: \(myTown.numberOfStoplights)")
