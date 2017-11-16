a6901ca3f0ec13e2045f1a9f7c02e528358e7d08
Jeff Ripke <jripke74@gmail.com>
2017-11-16 15:18:05 -0800

    MonsterTownIII: added lazy townSize property

--- MonsterTownIII/MonsterTownII/Town.swift
+++ MonsterTownIII/MonsterTownII/Town.swift
@@ -9,8 +9,26 @@
 import Foundation
 
 struct Town {
+    let region = "South"
     var population = 5_422
     var numberOfStoplights = 4
+    
+    enum Size {
+        case small
+        case medium
+        case large
+    }
+    
+    lazy var townSize: Size = {
+        switch self.population {
+        case 0...10_000:
+            return Size.small
+        case 10_001...100_000:
+            return Size.medium
+        default:
+            return Size.large
+        }
+    }()
     
     func printDescription() {
         print("Population: \(population); number of stoplights: \(numberOfStoplights)")
